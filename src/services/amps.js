bd30339d156711ea578464711456f987f08aac51
dcollis92 <davidphilipcollis@gmail.com>
2022-04-12 13:10:27 -0500

    create services for pedals+amps+guitars

---
+++ src/services/amps.js
@@ -0,0 +1,64 @@
+import * as tokenService from './tokenService'
+const BASE_URL = `${process.env.REACT_APP_API_URL}/api/amps/`
+
+export const create = async (amp) => {
+  try {
+    const res = await fetch(BASE_URL, {
+      method: "POST",
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${tokenService.getToken()}`
+      },
+      body: JSON.stringify(amp),
+    })
+    return await res.json()
+  } catch (error) {
+    throw error
+  }
+}
+
+export const getAll = async () => {
+  try {
+    const res = await fetch(`${BASE_URL}`)
+    return await res.json()
+  } catch (error) {
+    throw error
+  }
+}
+
+export const getOne = async (id) => {
+  try {
+    const res = await fetch(`${BASE_URL}${id}`)
+    return await res.json()
+  } catch (error) {
+    throw error
+  }
+}
+
+export const update = async (amp) => {
+  try {
+    const res = await fetch(`${BASE_URL}${amp.id}`, {
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${tokenService.getToken()}`
+      },
+      body: JSON.stringify(amp)
+    })
+    return await res.json()
+  } catch (error) {
+    throw error
+  }
+}
+
+export const deleteOne = async (id) => {
+  try {
+    const res = await fetch(`${BASE_URL}${id}`, {
+      method: "DELETE",
+      headers: { 'Authorization': `Bearer ${tokenService.getToken()}` }
+    })
+    return await res.json()
+  } catch (error) {
+    throw error
+  }
+}
