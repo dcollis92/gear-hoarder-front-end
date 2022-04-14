3b674d69b6dfb7e15dbc5c9ce6644a1329d08f44
dcollis92 <davidphilipcollis@gmail.com>
2022-04-14 00:02:04 -0500

    adjust rig input

--- src/pages/Forms/RigInput.jsx
+++ src/pages/Forms/RigInput.jsx
@@ -4,11 +4,11 @@ const RigInput = ({ form, handleChange }) => {
       <label htmlFor="name">Name</label>
       <input
         value={form.name ? form.name : ''} onChange={handleChange} id="name"
-        required name="name" type="text" placeholder="Name" autoComplete="off" />
+        required name="name" type="text" placeholder="Black Sabbath rig" autoComplete="off" />
       <label htmlFor="description">Description</label>
       <input
         value={form.description ? form.description : ''} onChange={handleChange} id="description"
-        required name="description" type="text" placeholder="Description" autoComplete="off" />
+        required name="description" type="text" placeholder="Used by Iommi from 1970-1973..." autoComplete="off" />
     </>
   )
 }
