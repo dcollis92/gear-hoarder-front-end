93803ea47c3a0ef3b079534a65b010a2fb5813d5
dcollis92 <davidphilipcollis@gmail.com>
2022-04-13 23:52:21 -0500

    adjust pedal input

--- src/pages/Forms/PedalInput.jsx
+++ src/pages/Forms/PedalInput.jsx
@@ -3,28 +3,25 @@ const PedalInput = ({ form, handleChange }) => {
     <>
       <label htmlFor="type">Type</label>
       <input
-        value={form.type ? form.type : ''} onChange={handleChange} id="type"
-        required name="type" type="text" placeholder="type" autoComplete="off" />
+        value={form.type ? form.type : ''} onChange={handleChange} id="type" required name="type" type="text" placeholder="Overdrive" autoComplete="off" />
       <label htmlFor="make">Make</label>
       <input
-        value={form.make ? form.make : ''} onChange={handleChange} id="make"
-        required name="make" type="text" placeholder="Make" autoComplete="off" />
+        value={form.make ? form.make : ''} onChange={handleChange} id="make" required name="make" type="text" placeholder="Ibanez" autoComplete="off" />
       <label htmlFor="model">Model</label>
       <input
-        value={form.model ? form.model : ''} onChange={handleChange} id="model"
-        required name="model" type="text" placeholder="Model" autoComplete="off" />                
+        value={form.model ? form.model : ''} onChange={handleChange} id="model" required name="model" type="text" placeholder="TS9 Tube Screamer" autoComplete="off" />                
       <label htmlFor="color">Color</label>
       <input
-        value={form.color ? form.color : '#ff0000'} onChange={handleChange} id="color"
+        value={form.color ? form.color : '#000000'} onChange={handleChange} id="color"
         required name="color" type="color" placeholder="Color" autoComplete="off" />
       <label htmlFor="year">Year</label>
       <input
-        value={form.year ? form.year : '1969'} onChange={handleChange} id="year"
-        required name="year" type="year" placeholder="year" autoComplete="off" />
+        value={form.year ? form.year : ''} onChange={handleChange} id="year" required name="year" type="year" placeholder="1969" 
+        autoComplete="off" />
       <label htmlFor="description">Description</label>
       <input
         value={form.description ? form.description : ''} onChange={handleChange} id="description"
-        required name="description" type="text" placeholder="Description" autoComplete="off" />
+        required name="description" type="text" placeholder="First Generation, Made in Japan" autoComplete="off" />
       <label htmlFor="isWorking">Broken?</label>
       <input
         checked={form.isWorking === 'true' ? true : false} onChange={handleChange} id="isWorking" type="checkbox" autoComplete="off" name="isWorking" />
