1f088a414f54de4a6d01dbc77c53e2894a12a147
Tim Crisp <timothycrisp@gmail.com>
2022-04-14 07:37:32 -0500

    normalize pedal and rig input fields

--- src/pages/Forms/RigInput.jsx
+++ src/pages/Forms/RigInput.jsx
@@ -3,12 +3,22 @@ const RigInput = ({ form, handleChange }) => {
     <>
       <label htmlFor="name">Name</label>
       <input
-        value={form.name ? form.name : ''} onChange={handleChange} id="name"
-        required name="name" type="text" placeholder="Black Sabbath rig" autoComplete="off" />
+        value={form.name ? form.name : ''} 
+        onChange={handleChange} 
+        id="name" required 
+        name="name" 
+        type="text" 
+        placeholder="Black Sabbath rig" 
+        autoComplete="off" />
       <label htmlFor="description">Description</label>
       <input
-        value={form.description ? form.description : ''} onChange={handleChange} id="description"
-        required name="description" type="text" placeholder="Used by Iommi from 1970-1973..." autoComplete="off" />
+        value={form.description ? form.description : ''} 
+        onChange={handleChange} 
+        id="description" required 
+        name="description" 
+        type="text" 
+        placeholder="Used by Iommi from 1970-1973..." 
+        autoComplete="off" />
     </>
   )
 }
