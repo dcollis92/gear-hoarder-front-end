e371aa9f585e8d0b2242ef2943832890b2dbcd9a
David Collis <95148980+dcollis92@users.noreply.github.com>
2022-04-14 23:47:05 -0500

    Update RigDetails.jsx

--- src/pages/RigDetails/RigDetails.jsx
+++ src/pages/RigDetails/RigDetails.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { useParams, useNavigate } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import './RigDetails.css'
 
 // Services
