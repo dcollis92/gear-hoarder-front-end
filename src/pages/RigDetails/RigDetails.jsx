b0a5addeafdce9be728d043ba541837169937d87
provideforme <evan.a.bernard@gmail.com>
2022-04-14 21:35:32 -0700

    ternary returns in action pages

--- src/pages/RigDetails/RigDetails.jsx
+++ src/pages/RigDetails/RigDetails.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import './RigDetails.css'
 
 // Services
--- src/pages/RigDetails/components/RigActions.jsx
+++ src/pages/RigDetails/components/RigActions.jsx
@@ -3,11 +3,15 @@ import { useNavigate } from 'react-router-dom'
 const RigActions = ({ rig, user }) => {
   const navigate = useNavigate()
   return (
-    rig.profile_id === user.id &&
+    rig.profile_id === user.id
+    ?
     <div className="actions">
       <button className="btn warn" onClick={() => navigate(`/rigs/${rig.id}/edit`, { state: rig })}>Edit</button>
       <button className="btn danger" onClick={() => navigate(`/rigs/${rig.id}/confirmation`, { state: rig })}>Delete</button>
+      <button className="btn danger" onClick={() => navigate(`/rigs`)}>Return</button>
     </div>
+    :
+    <button className="btn danger" onClick={() => navigate(`/rigs`)}>Return</button>
   )
 }
 
